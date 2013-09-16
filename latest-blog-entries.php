a7f34fb82e2c627338f403767769266d7ff6f3c2
Simon Prosser <pross@pross.org.uk>
2013-09-16 19:58:49 +0100

    Update latest-blog-entries.php

--- latest-blog-entries.php
+++ latest-blog-entries.php
@@ -6,4 +6,5 @@ Author URI: http://enriquechavez.co
 Version: 1.1
 Description: Latest Blogs Entries is a very powerful section for Pagelines which displays your recent posts with thumbnail, excerpt, title, date and read more link . It’s the perfect solution to show specific entries on the home page or in any other page. With more that 15 options in general.
 Class Name: TmLatestBlog
+PageLines: true
 */
